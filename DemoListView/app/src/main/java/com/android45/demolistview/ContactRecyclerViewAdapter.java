f626c33b0eaa4b715fb1c4e7c51787e313c34179
Trương Trung Kiên <truongtrungkien2302@gmail.com>
2021-11-29 19:46:06 +0700

    RecyclerView

---
+++ DemoListView/app/src/main/java/com/android45/demolistview/ContactRecyclerViewAdapter.java
@@ -0,0 +1,53 @@
+package com.android45.demolistview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ContactRecyclerViewAdapter extends RecyclerView.Adapter<ContactRecyclerViewAdapter.Viewhoder> {
+    List<ContactDemo> contactDemoList;
+
+    public ContactRecyclerViewAdapter(List<ContactDemo> contactDemoList) {
+        this.contactDemoList = contactDemoList;
+    }
+
+    @NonNull
+    @Override
+    public ContactRecyclerViewAdapter.Viewhoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.icon_recyclerview, parent, false);
+
+        Viewhoder viewhoder = new Viewhoder(view);
+        return viewhoder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactRecyclerViewAdapter.Viewhoder holder, int position) {
+        ContactDemo contactDemo = contactDemoList.get(position);
+
+        holder.tvName.setText(contactDemo.getName());
+        holder.tvPhone.setText(String.valueOf(contactDemo.getPhoneNumber()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactDemoList.size();
+    }
+
+    public class Viewhoder extends RecyclerView.ViewHolder {
+        TextView tvName, tvPhone;
+
+        public Viewhoder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvNameRv);
+            tvPhone = itemView.findViewById(R.id.tvPhoneRv);
+        }
+    }
+}
--- DemoListView/app/src/main/java/com/android45/demolistview/RecyclerViewActivity.java
+++ DemoListView/app/src/main/java/com/android45/demolistview/RecyclerViewActivity.java
@@ -1,6 +1,7 @@
 package com.android45.demolistview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -9,15 +10,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RecyclerViewActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
+    RecyclerView rvList;
     List<ContactDemo> contactDemoList;
     ContactDemo contact01, contact02, contact03, contact04, contact05;
+    ContactRecyclerViewAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
-        recyclerView = findViewById(R.id.rvContact);
+        rvList = findViewById(R.id.rvContact);
 
         contactDemoList = new ArrayList<>();
         contactDemoList.add(new ContactDemo("Android", 99999, true));
@@ -26,5 +28,11 @@ public class RecyclerViewActivity extends AppCompatActivity {
         contactDemoList.add(new ContactDemo("PHP", 66666, false));
         contactDemoList.add(new ContactDemo("Flutter", 55555, true));
 
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getBaseContext(), 1, RecyclerView.HORIZONTAL, false);
+
+        recyclerViewAdapter = new ContactRecyclerViewAdapter(contactDemoList);
+
+        rvList.setLayoutManager(layoutManager);
+        rvList.setAdapter(recyclerViewAdapter);
     }
 }
